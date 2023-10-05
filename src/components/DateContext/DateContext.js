db6cad703372aab5ef73faf82025aa951852f7b9
Ash3-23 <nerea.trebol@yahoo.es>
2023-10-05 15:53:48 +0200

    dates correction

--- src/components/DateContext/DateContext.js
+++ src/components/DateContext/DateContext.js
@@ -16,22 +16,24 @@ export const DateProvider = ({ children }) => {
     console.log(newDates);
     console.log(setAvailableDates);
   };
-  const fetchAvailableDates = async () => {
-    try {
-      const response = await apiServiceInstance.getAvailableDates();
-      const datesFromServer = response.data.availableDates;
-      setAvailableDates(datesFromServer);
-    } catch (error) {
-      console.error('Error al obtener las fechas disponibles:', error);
-    }
-  };
+  // const fetchAvailableDates = async () => {
+  //   try {
+  //     const response = await apiServiceInstance.getAvailableDates();
+  //     const datesFromServer = response.data.availableDates;
+  //     setAvailableDates(datesFromServer);
+  //   } catch (error) {
+  //     console.error('Error al obtener las fechas disponibles:', error);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchAvailableDates();
-  }, []);
+  // useEffect(() => {
+  //   fetchAvailableDates();
+  // }, []);
 
   return (
-    <DateContext.Provider value={{ availableDates, setAvailableDates, setDates, fetchAvailableDates }}>
+    <DateContext.Provider value={{ availableDates, setAvailableDates, setDates,
+    // fetchAvailableDates
+     }}>
       {children}
     </DateContext.Provider>
   );
